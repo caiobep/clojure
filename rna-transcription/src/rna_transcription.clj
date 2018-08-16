ebb1c11251999dccbc94c867fd4878ee31c65a9a
Caio Amaral Corrêa <caiobep@me.com>
2018-08-16 16:12:26 -0300

    Better Implementation of RNA transcriptions

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -1,18 +1,17 @@
 (ns rna-transcription
   (:require [clojure.string :refer [split join]]))
 
-(defn is-valid-dna? [a]
-  (contains? #{"A" "G" "T" "C"} a))
+(def substitutions {\G \C
+                    \C \G
+                    \T \A
+                    \A \U})
+
+
 
 (defn to-rna [dna]
   "Transcribe a DNA to RNA"
-   (let [dna-char (split dna #"")]
-        (when (some false? (map is-valid-dna? dna-char))
-          (throw (AssertionError. "Wrong input.")))
-        (->> dna-char
-          (map #(case %
-                  "C" "G"
-                  "G" "C"
-                  "A" "U"
-                  "T" "A"))
-          (join))))
+  (apply str
+        (map (fn [a]
+               (assert (contains? substitutions a))
+               (get substitutions a))
+            dna)))
